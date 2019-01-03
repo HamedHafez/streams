1c7e59412d3d9664e734118a1adb53e87c8c1196
Hamed Hafezniya <hafezniyaa@gmail.com>
2019-01-03 12:29:50 +0330

    implement editstream and create stream

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './Streams/StreamCreate';
 import StreamDelete from './Streams/StreamDelete';
 import StreamEdit from './Streams/StreamEdit';
 import StreamList from './Streams/StreamList';
 import StreamShow from './Streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 
 const App = () => {
     return (
-        <div className="ui container">
-            <BrowserRouter>
+        <div className="ui container" style={{ marginTop: 20 }}>
+            <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList}>List</Route>
-                    <Route path="/streams/new" component={StreamCreate}>Creat</Route>
-                    <Route path="/streams/delete" component={StreamDelete}>Delete</Route>
-                    <Route path="/streams/edit" component={StreamEdit}>Edit</Route>
-                    <Route path="/streams/show" component={StreamShow}>Show</Route>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 }
