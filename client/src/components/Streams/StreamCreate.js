8e4bba6f517d81d1bc0a9e12e20727d6dcc167e6
Hamed Hafezniya <hafezniyaa@gmail.com>
2019-01-05 19:12:17 +0330

    implement streamshow

--- client/src/components/Streams/StreamCreate.js
+++ client/src/components/Streams/StreamCreate.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../Actions';
+import { createStream } from '../../Actions';
 import StreamForm from './StreamForm';
 
-
 class StreamCreate extends React.Component {
-
-
     onSubmit = (formvalues) => {
         this.props.createStream(formvalues);
     }
@@ -19,6 +16,4 @@ class StreamCreate extends React.Component {
        ); 
     }
 }
-
-
-export default connect(null, { fetchStream })(StreamCreate);
+export default connect(null, { createStream })(StreamCreate);
